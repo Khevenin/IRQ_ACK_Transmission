d8dadde8ab2bae49aec966e553d7570c3c69a279
Khevenin <wojciechbernat95@gmail.con>
2019-12-14 20:45:07 +0100

    Create file to lib-adc-meas

---
+++ Remote/lib-adc-meas.cpp
@@ -0,0 +1,17 @@
+/**
+ * File with funtions to execute specialized measure tasks.
+ * Based on external function from lib-adc-irq.
+ * @Khevenin
+ * */
+
+#include "lib-adc-meas.h"
+
+unsigned short adcTab[MEASURE_SIZE];
+
+void adcMeasure(unsigned short *tab, size_t tabSize)
+{
+}
+
+ISR(ADC_vect)
+{
+}
---
+++ Remote/lib-adc-meas.h
@@ -0,0 +1,9 @@
+/**
+ * File with funtions to execute specialized measure tasks.
+ * Based on external function from lib-adc-irq.
+ * @Khevenin
+ * */
+
+#define MEASURE_SIZE 8
+
+extern unsigned short adcTab;
