557367f4ba49189b3af189dfc8fbcd779e63c71a
Khevenin <wojciechbernat95@gmail.con>
2019-12-14 21:13:20 +0100

    Added adcMeasure and meanVal function

--- Remote/lib-adc-meas.cpp
+++ Remote/lib-adc-meas.cpp
@@ -6,11 +6,37 @@
 
 #include "lib-adc-meas.h"
 
-unsigned short adcTab[MEASURE_SIZE];
+unsigned short volatile adcTab[MEASURE_SIZE];
+unsigned short volatile measureCounter = 0;
+unsigned short resetCounter = 0x08;
 
-void adcMeasure(unsigned short *tab, size_t tabSize)
+void adcMeasure(unsigned short volatile *tab, unsigned short volatile *counter)
+{
+    tab[*counter] = readAdcEight();
+    *counter++;
+    *counter = *counter & resetCounter; //reste measureCounter value using bitwise AND
+}
+
+unsigned short meanVal(unsigned volatile *tab, size_t size)
 {
 
+    if (size)
+    {
+        unsigned float tmp = 0;
+
+        size_t i;
+        for (i = 0; i < size; i++)
+        {
+            tmp += tab[i];
+        }
+
+        tmp = tmp / size;
+        return short(tmp);
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 ISR(ADC_vect)
--- Remote/lib-adc-meas.h
+++ Remote/lib-adc-meas.h
@@ -4,6 +4,13 @@
  * @Khevenin
  * */
 
+#include "lib-adc-irq.h"
+
 #define MEASURE_SIZE 8
 
 extern unsigned short adcTab;
+extern unsigned short volatile measureCounter;
+extern unsigned short resetCounter;
+
+void adcMeasure(unsigned short volatile *tab, unsigned short volatile *counter);
+unsigned short meanVal(unsigned volatile *tab, size_t size);
