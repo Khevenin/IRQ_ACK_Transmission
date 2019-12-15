b7c189c853b5bf8ad5c1cfbffce5309a3e0721f9
Khevenin <wojciechbernat95@gmail.con>
2019-12-15 12:36:43 +0100

    Added main program file
    ACK_IRQ_Trans_Main

---
+++ Remote/main/ACK_IRQ_Trans_Main/ACK_IRQ_Trans_Main.ino
@@ -0,0 +1,22 @@
+/**
+ * IRQ_ACK_Transmission
+ * Main program file.
+ * Created by Arduino IDE
+ * External files: 
+ * lib-adc-irq.h
+ * lib-adc-meas.h
+ * @autor: Khevenin
+ * @date: 15-12-2019
+ */
+
+#include 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
