b6c856b4f78562d24a0a08a5095062c94929363b
Khevenin <wojciechbernat95@gmail.con>
2019-12-19 21:43:10 +0100

    Started initialization of radio module

--- Remote/main/ACK_IRQ_Trans_Main/ACK_IRQ_Trans_Main.ino
+++ Remote/main/ACK_IRQ_Trans_Main/ACK_IRQ_Trans_Main.ino
@@ -10,15 +10,35 @@
 */
 
 #include <Arduino.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <nRF24l01.h>
+#include <printf.h>
+
 #include "libbuf.h"
+#include "lib-adc-irq.h"
 
-#define BUF_SIZE 32
+#define CE 7
+#define CSN 8
 
+#define TX_BUF_SIZE 32
+#define RX_BUF_SIZE 32
+
+
+
+RF24 Remote(CE, CSN); //set CE, CSN pins
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Serial port init.");
+  Serial.println("RF init start.");
+  Remote.begin();
+  Remote.setPALevel(RF24_PA_LOW);
+  Remote.setDataRate(RF24_250KBPS);
+  radio.setChannel(0x64);
+  radio.setRetries(4, 3);
+  radio.enableAckPayload();
 
 
 }
